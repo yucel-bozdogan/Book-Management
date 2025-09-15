16f2f739729c0d185c5e5a0fa084d14f23b70621
yucel-bozdogan <yucel.bozdogan@outlook.com>
2025-09-15 21:01:58 +0300

    patch indexts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,9 @@
 import express from 'express';
 import booksRouter from './books';
-
+import { processIdGeneration } from '../middlewares/processIdGeneration';
 const router = express.Router();
 
+router.use(processIdGeneration); //processIdGeneration middleware'i tüm route'lara uygulanır
 
 router.use('/books', booksRouter);
 
